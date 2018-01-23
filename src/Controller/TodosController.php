b1226e6e128bf06e1873d78d27b573097355b3dd
Drunken-Dragon <janziolo@gmail.com>
2018-01-23 11:40:09 +0100

    Created more routes in TodosController and new twig templates for todos.

--- src/Controller/TodosController.php
+++ src/Controller/TodosController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,5 +15,26 @@ class TodosController extends Controller
     public function todosList()
     {
         return $this->render('base.html.twig');
+    }
+    /**
+     * @Route("/todos/new")
+     */
+    public function todosNew(Request $request)
+    {
+        return $this->render('todos/new.html.twig');
+    }
+    /**
+     * @Route("/todos/edit/{id}")
+     */
+    public function todosEdit($id, Request $request)
+    {
+        return $this->render('todos/edit.html.twig');
+    }
+    /**
+     * @Route("/todos/details/{id}")
+     */
+    public function todosDetails($id)
+    {
+        return $this->render('todos/details.html.twig');
     }
 }
